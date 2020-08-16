06f401262e55d330682b929238e4ce060b04d17e
Keerthana <Keerthana@LAPTOP-6VH2RU3V>
2020-08-16 20:35:09 +0530

    https://github.com/jayashri14/guess-the-number.git

---
+++ src/guess/the/number/GuessTheNumber.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guess.the.number;
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+public class GuessTheNumber 
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Random rndm_method = new Random(); 
+        double n,snum;
+        n=(Math.random()*99999);
+        long a=Math.round(n);
+        System.out.print(a);
+        snum=sc.nextDouble();
+        
+        if(a>snum){
+             System.out.println("lower");
+        }
+        else if(a==snum){
+            System.out.println("equal");
+        }
+        else{
+             System.out.println("higher");   
+        }   
+    }
+}
+        
+       
+
